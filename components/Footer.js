25a448075ff814bb0be6522a03e7866d9bc04404
gurazadas <satyasai.gurazada@gmail.com>
2026-04-03 19:07:11 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+// components/Footer.js
+export function Footer() {
+  return (
+    <footer className="bg-gray-100 py-8 text-center">
+      <p className="text-gray-600">
+        © 2026 Satyasai | Data Vault Architect
+      </p>
+    </footer>
+  );
+}
